3bd7a03387d5b92d41f5d959c5baacae9dcdc9ca
Monroe100 <michelle1hunnid@gmail.com>
2018-06-26 10:43:11 +0300

    fixed a few bugs that affected the local host

--- nay/urls.py
+++ nay/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
+from django.conf import settings
+from django.contrib.auth import views
+from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^watch/',include('watch.urls')),
-    if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
