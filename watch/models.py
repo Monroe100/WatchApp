d2dbff80c4e6f55807a2d5cc08dac59f4ac0cc59
Monroe100 <michelle1hunnid@gmail.com>
2018-06-26 15:35:20 +0300

    made the neighbourhood class in models.py

--- watch/models.py
+++ watch/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Neighbourhood(models.Model):
+    neighbourhood_name = models.CharField(max_length =30)
+    neighbourhood_location = models.CharField(max_length = 30)
+    occupants_count = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
