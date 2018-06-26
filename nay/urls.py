6e91ec2c7f9761520b3d53ca9103a6f26dd98966
Monroe100 <michelle1hunnid@gmail.com>
2018-06-26 10:23:44 +0300

    set up the Email configurations so as to allow sending of emails

--- nay/urls.py
+++ nay/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^watch/',include('watch.urls')),
+    if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 ]
