5b73f8d15d1c6b975256b7ba1f29d25018a379b7
Monroe100 <michelle1hunnid@gmail.com>
2018-06-26 15:42:16 +0300

    created delete,create and get method in models

--- watch/models.py
+++ watch/models.py
@@ -9,3 +9,25 @@ class Neighbourhood(models.Model):
     occupants_count = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
+    def __str__(self):
+        return self.neighbourhood_name
+
+    def create_neighbourhood(self):
+        '''Method to create a neighbourhood'''
+        self.create()
+
+    def delete_neighbourhood(self):
+        '''Method to delete a neighbourhood'''
+        self.delete()
+
+    @classmethod
+    def get_neighbourhoods(cls):
+        '''
+        Method that gets all image posts from the database
+        Returns:
+            get_posts : list of image post objects from the database
+        ''' 
+        Neighbourhoods = cls.objects.all()
+        return Neighbourhoods
+
