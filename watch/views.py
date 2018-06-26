833325c5f1c7a67e02388ed083fea370302e1f05
Monroe100 <michelle1hunnid@gmail.com>
2018-06-26 11:38:22 +0300

    fixed a fe bugs

--- watch/views.py
+++ watch/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def welcome(request):
